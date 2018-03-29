1cf39ebc5b0e8b76031cedced2fb2c224a24b8e5
vagrant <andhdo@gmail.com>
2018-03-28 21:02:28 -0500

    remove trailing spaces

--- test/metabase/api/public_test.clj
+++ test/metabase/api/public_test.clj
@@ -188,7 +188,7 @@
   (tu/with-temporary-setting-values [enable-public-sharing true]
     (with-temp-public-card [{uuid :public_uuid}]
       (http/client :get 200 (str "public/card/" uuid "/query/pdf")))))
-      
+
 ;; Check that we can exec a PublicCard with `?parameters`
 (expect
   [{:name "Venue ID", :slug "venue_id", :type "id", :value 2}]
